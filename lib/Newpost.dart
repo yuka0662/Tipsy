08bbce8e4c6642cf376b1e77810f0e7b23d623e9
Ak0iK5m <ca05tt6i@gmail.com>
2021-06-04 16:43:01 +0900

    画像挿入コード書いてる途中

--- lib/Newpost.dart
+++ lib/Newpost.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import './Header.dart';
+
 
 class Newpost extends StatelessWidget {
   @override
@@ -7,8 +10,39 @@ class Newpost extends StatelessWidget {
     return Scaffold(
       appBar: Bar_nav(),
     );
-    return Text(
-      'Newpost',
-    );
+
   }
 }
+
+
+// class ImagePicker{
+  
+//   File _image;
+//   final picker = ImagePicker();
+
+
+//   //ギャラリーから画像を取得
+//   Future getImageFromGallery() async {
+
+//     final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+//     setState((){
+//       _image = File(pickedFile.path);
+//     });
+//   }
+
+//   body :Center(
+//     child: Column(
+//       crossAxisAlignment: CrossAxisAlignment.center,
+//          children: [
+//            Padding(
+//              padding: const EdgeInsets.all(8.0),
+//              child: Container(
+//                  width: 300,
+//                  child: _image == null
+//                      ? Text('No image selected.')
+//                      : Image.file(_image)
+//     )
+//   )
+
+//}
