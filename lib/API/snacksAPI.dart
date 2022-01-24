09f176bbcf5bb07a1ec51512efa76943105afb0a
yuka0662 <arupaka151@yahoo.co.jp>
2022-01-24 11:42:17 +0900

    おすすめのおつまみ表示、画面作成

--- lib/API/snacksAPI.dart
+++ lib/API/snacksAPI.dart
@@ -1,145 +1,151 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-//おつまみ一覧表示
-class SnacksAll extends StatefulWidget {
-  SnacksAll(this._category);
-  final String _category;
-
+class OrecipeDetail extends StatefulWidget {
   @override
-  _SnackState createState() => new _SnackState(_category);
+  final int _index;
+  final String _doc;
+  OrecipeDetail(this._index, this._doc);
+  _OrecipeDetailState createState() => new _OrecipeDetailState(_index, _doc);
 }
 
-class _SnackState extends State<SnacksAll> {
-  _SnackState(this._category);
-  final String _category;
-  var category;
+//おつまみ作り方表示ページ
+class _OrecipeDetailState extends State {
+  final int _index;
+  final String _doc;
+  _OrecipeDetailState(this._index, this._doc);
 
-  Map data;
-  List useData;
+  List snackData;
+  List materials;
+  List procedures;
+  Future getSnackData() async {
+    await for (var snapshot
+        in FirebaseFirestore.instance.collection('snacks').snapshots()) {
+      snackData = [];
+      for (var snack in snapshot.docs) {
+        setState(() {
+          snackData.add(snack.data());
+        });
+      }
+    }
+  }
 
-  Future getData() async {
-    
+  Future getMaterial() async {
+    await for (var snapshot in FirebaseFirestore.instance
+        .collection('snacks')
+        .doc(_doc)
+        .collection('material')
+        .orderBy('name')
+        .snapshots()) {
+      materials = [];
+      for (var material in snapshot.docs) {
+        setState(() {
+          materials.add(material.data());
+        });
+      }
+    }
+  }
+
+  Future getProcedure() async {
+    await for (var snapshot in FirebaseFirestore.instance
+        .collection('snacks')
+        .doc(_doc)
+        .collection('procedure')
+        .snapshots()) {
+      procedures = [];
+      for (var procedure in snapshot.docs) {
+        setState(() {
+          procedures.add(procedure.data());
+        });
+      }
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getSnackData();
+    getMaterial();
+    getProcedure();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
-      //スクロール可能な可変リストを作る
-      itemCount: useData == null ? 0 : useData.length, //受け取る数の定義
-      itemBuilder: (BuildContext context, int index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => OrecipeDetail()));
-          },
-          child: Card(
-            //cardデザインを定義:material_design
-            child: Row(
-              children: <Widget>[
-                CachedNetworkImage(
-                  width: 150,
-                  height: 150,
-                  imageUrl:
-                      'https://dm58o2i5oqos8.cloudfront.net/photos/${useData[index]["cocktail_id"]}.jpg',
-                  errorWidget: (conte, url, dynamic error) =>
-                      Image.asset('assets/InPreparation_sp.png'),
-                ),
-                Flexible(
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Text(
-                          useData[index]["cocktail_digest"],
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          useData[index]["cocktail_name"],
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                    ],
+    if (materials == null) {
+      return Scaffold(
+        body: Text(''),
+      );
+    } else {
+      return ListView(children: [
+        CachedNetworkImage(
+          imageUrl: snackData[_index]['image'],
+          errorWidget: (conte, url, dynamic error) =>
+              Image.asset('assets/InPreparation_sp.png'),
+        ),
+        Text('${snackData[_index]['name']}\n',
+            style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 5.0,
+                color: Colors.black,
+                decoration: TextDecoration.none)),
+        Text('材料(${snackData[_index]['num']}人分)',
+            style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 5.0,
+                color: Colors.black,
+                decoration: TextDecoration.none)),
+        for (int i = 0; i < materials.length - 1; i++)
+          Container(
+            decoration: BoxDecoration(
+                border: const Border(
+                    bottom: BorderSide(
+              color: Colors.grey,
+              width: 1,
+            ))),
+            child: ListTile(
+                title: Row(
+              children: [
+                Container(
+                  width: 250,
+                  child: Text(
+                    materials[i]["name"],
                   ),
                 ),
-                IconButton(
-                  onPressed: () => {},
-                  icon: Icon(
-                    Icons.favorite_border,
-                    color: Colors.black,
-                  ),
-                ),
+                Text(materials[i]["amount"])
               ],
-            ),
+            )),
           ),
-        );
-      },
-    );
-  }
-}
-
-//おつまみ作り方表示ページ
-class OrecipeDetail extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(children: [
-      Text('だし巻き卵\n\n調理時間：15分\nきれいな仕上がり◎おかずにぴったりなだし巻き卵♪\n',
-          style: TextStyle(
-              fontSize: 15.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-      Text('材料(4人分)',
-          style: TextStyle(
-              fontSize: 20.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-      Text(
-          '卵　　　　　　　　　　　　：4個\nⒶ水　　　　　　　　　　　：大さじ6\nⒶ「ほんだし」　　　　　　：小さじ1/2\nⒶみりん　　　　　　　　　：小さじ2\nⒶ薄口しょうゆ　　　　　　：小さじ1\n「AJINOMOTO サラダ油」：適量\n大根おろし・お好みで　　　：適量\n',
-          style: TextStyle(
-              fontSize: 15.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-      Text('手順',
-          style: TextStyle(
-              fontSize: 20.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-      Text(
-          '1. ボウルに卵を割りほぐし、白身を切るようによく溶く。混ぜ合わせたＡを加えてさらに混ぜる。\n\n2. 卵焼き器を強めの中火で熱し、油を含ませたキッチンペーパーで全体に油をなじませる。（１）の卵液を玉じゃくし七分目ほど入れて広げ、半熟になったら向こう側から手前に向かって巻き、巻いた卵を向こう側に送る。\n\n3. 卵焼き器に油をなじませ、再び卵液を流し入れる。巻いた卵を持ち上げて下に卵液を流し、卵のフチが固まってきたら手前に巻き込む。同様にくり返し焼く。\n\n4. 焼き上がったらまな板に取り出し、粗熱が取れたら食べやすい大きさに切る。\n\n5. 器に盛り、好みで大根おろしを添える。\n',
-          style: TextStyle(
-              fontSize: 15.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-      Text('メモ',
-          style: TextStyle(
-              fontSize: 20.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-      Text('コツ・ポイント\n＊お弁当に入れる際は、Ａに塩少々を加えてください。',
-          style: TextStyle(
-              fontSize: 15.0,
-              letterSpacing: 5.0,
-              color: Colors.black,
-              decoration: TextDecoration.none)),
-    ]);
+        Text('\n手順',
+            style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 5.0,
+                color: Colors.black,
+                decoration: TextDecoration.none)),
+        for (int i = 0; i < procedures.length; i++)
+          Container(
+            child: Text('${i + 1}. ${procedures[i]['detail']}\n',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 5.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                    decoration: TextDecoration.none)),
+          ),
+        Text('メモ\n',
+            style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 5.0,
+                color: Colors.black,
+                decoration: TextDecoration.none)),
+        Text('${snackData[_index]['comment']}\n',
+            style: TextStyle(
+                fontSize: 16.0,
+                letterSpacing: 5.0,
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none)),
+      ]);
+    }
   }
 }
--- lib/Top.dart
+++ lib/Top.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import './API/snacksAPI.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
@@ -44,111 +46,180 @@ class _TopState extends State {
     }
   }
 
+  List snackData;
+  List docs;
+  Future getSnackData() async {
+    await for (var snapshot
+        in FirebaseFirestore.instance.collection('snacks').snapshots()) {
+      snackData = [];
+      docs = [];
+      for (var snack in snapshot.docs) {
+        setState(() {
+          snackData.add(snack.data());
+          docs.add(snack.id);
+        });
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     getData();
+    getSnackData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: <Widget>[
-      CarouselSlider.builder(
-        options: CarouselOptions(
-          initialPage: 0,
-          viewportFraction: 1,
-          enableInfiniteScroll: true,
-          autoPlay: true,
+    if (snackData == null) {
+      return Scaffold(
+        body: Text(''),
+      );
+    } else {
+      return ListView(children: <Widget>[
+        CarouselSlider.builder(
+          options: CarouselOptions(
+            initialPage: 0,
+            viewportFraction: 1,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+          ),
+          itemCount: images == null ? 0 : 3,
+          itemBuilder: (BuildContext context, int index, int index2) {
+            return GestureDetector(
+                onTap: () => {
+                      launch(siteURL[index]),
+                    },
+                child: Image.asset(images[index]));
+          },
         ),
-        itemCount: images == null ? 0 : 3,
-        itemBuilder: (BuildContext context, int index, int index2) {
-          return GestureDetector(
-              onTap: () => {
-                    launch(siteURL[index]),
-                  },
-              child: Image.asset(images[index]));
-        },
-      ),
-      Container(),
-      Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-        child: Flexible(
-            child: Text(
-          '人気のお酒ランキング',
-          style: TextStyle(
-              fontSize: 20, color: HexColor('212738'), wordSpacing: 20),
-        )),
-      ),
-      Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-        child: Flexible(
-          child: Container(
-              height: 500,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                //スクロール可能な可変リストを作る
-                itemCount: useData == null ? 0 : 10, //受け取る数の定義
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
+        Padding(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Flexible(
+              child: Text(
+            'おすすめのおつまみ',
+            style: TextStyle(
+                fontSize: 20, color: HexColor('212738'), wordSpacing: 20),
+          )),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+          child: CarouselSlider.builder(
+            options: CarouselOptions(
+              height: 210,
+              initialPage: 0,
+              viewportFraction: 0.5,
+              enableInfiniteScroll: true,
+              autoPlay: false,
+            ),
+            itemCount: snackData == null ? 0 : snackData.length,
+            itemBuilder: (BuildContext context, int index, int index2) {
+              return GestureDetector(
+                  onTap: () => {
+                        Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => TaskDetail(
-                                  useData[index]["Item"]["mediumImageUrls"][0]
-                                      ["imageUrl"],
-                                  useData[index]["Item"]["itemName"],
-                                  int.parse(
-                                      useData[index]["Item"]["itemPrice"]),
-                                  useData[index]["Item"]["itemUrl"],
-                                  useData[index]["Item"]["itemCaption"])));
-                    },
-                    child: Card(
-                      //cardデザインを定義:material_design
-                      child: Row(
-                        children: <Widget>[
-                          Text('第' + (index + 1).toString() + '位'),
-                          CachedNetworkImage(
-                            width: 150,
-                            height: 150,
-                            imageUrl: useData[index]["Item"]["mediumImageUrls"]
-                                [0]["imageUrl"],
-                            errorWidget: (conte, url, dynamic error) =>
-                                Image.asset('assets/InPreparation_sp.png'),
+                            builder: (context) =>
+                                OrecipeDetail(index, docs[index]),
                           ),
-                          Flexible(
-                            child: Column(
-                              children: [
-                                Center(
-                                  child: Text(
-                                    useData[index]["Item"]["itemName"],
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                                Center(
-                                  child: Text(
-                                    NumberFormat()
-                                            .format(int.parse(useData[index]
-                                                ["Item"]["itemPrice"]))
-                                            .toString() +
-                                        '円',
-                                    style: TextStyle(
-                                        color: Colors.red, fontSize: 17),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+                        ),
+                      },
+                  child: Column(
+                    children: [
+                      CachedNetworkImage(
+                        height: 150,
+                        imageUrl: snackData[index]['image'],
+                        errorWidget: (conte, url, dynamic error) =>
+                            Image.asset('assets/InPreparation_sp.png'),
                       ),
-                    ),
-                  );
-                  //Row(children: [child:],),
-                },
-              )),
+                      Text(
+                        snackData[index]['name'],
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ));
+            },
+          ),
         ),
-      ),
-    ]);
+        Padding(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Flexible(
+              child: Text(
+            '人気のお酒ランキング',
+            style: TextStyle(
+                fontSize: 20, color: HexColor('212738'), wordSpacing: 20),
+          )),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Flexible(
+            child: Container(
+                height: 500,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                  //スクロール可能な可変リストを作る
+                  itemCount: useData == null ? 0 : 10, //受け取る数の定義
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => TaskDetail(
+                                    useData[index]["Item"]["mediumImageUrls"][0]
+                                        ["imageUrl"],
+                                    useData[index]["Item"]["itemName"],
+                                    int.parse(
+                                        useData[index]["Item"]["itemPrice"]),
+                                    useData[index]["Item"]["itemUrl"],
+                                    useData[index]["Item"]["itemCaption"])));
+                      },
+                      child: Card(
+                        //cardデザインを定義:material_design
+                        child: Row(
+                          children: <Widget>[
+                            Text('第' + (index + 1).toString() + '位'),
+                            CachedNetworkImage(
+                              width: 150,
+                              height: 150,
+                              imageUrl: useData[index]["Item"]
+                                  ["mediumImageUrls"][0]["imageUrl"],
+                              errorWidget: (conte, url, dynamic error) =>
+                                  Image.asset('assets/InPreparation_sp.png'),
+                            ),
+                            Flexible(
+                              child: Column(
+                                children: [
+                                  Center(
+                                    child: Text(
+                                      useData[index]["Item"]["itemName"],
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      NumberFormat()
+                                              .format(int.parse(useData[index]
+                                                  ["Item"]["itemPrice"]))
+                                              .toString() +
+                                          '円',
+                                      style: TextStyle(
+                                          color: Colors.red, fontSize: 17),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )),
+          ),
+        ),
+      ]);
+    }
   }
 }
